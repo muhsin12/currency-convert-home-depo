d9ff6f1bd4be474a977b8a13a03ef83a95e5b492
muhsin12 <muhsin.mp@gmail.com>
2019-07-01 11:09:23 +0530

    Update exchange.service.ts

--- src/app/service/exchange.service.ts
+++ src/app/service/exchange.service.ts
@@ -12,7 +12,7 @@ export class ExchangeService {
   
   // RESTful API  and HTTP headers
   EXCHANGE_API = 'http://data.fixer.io/api/latest?access_key=0e82e246500e79435b1d3aa892835d1b&symbols=CAD,USD,EUR&format=1';
-  /*EXCHANGE_API = 'http://data.fixer.io/api/latest?access_key=052ed2a09fe56cae04ebe56831ca960f&symbols=CAD,USD,EUR&format=1';
+/*
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json'
